d92e9cca2e53928f5a31b3d09f4c21abda50b2f4
thisisanish <email.anishagarwal@gmail.com>
2020-08-19 14:45:03 +0530

    fixed

---
+++ Classwork_2/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Display from './components/Display';
+import Navbar from './components/Navbar';
+import AddCourse from './components/AddCourse';
+import './App.css';
+
+class App extends React.Component {
+  state = { 
+    heading: 'React App',
+    courseDetails: [
+      { id: 1, title: 'React', details: 'Best UI Library ', instructor: 'Jack' },
+      { id: 2, title: 'Node', details: 'I/O, event driven environment', instructor: 'Jack' },
+    ]
+   }
+
+   addCourse = (course) => {
+     console.log('Inside App.js', course);
+     course.id = Math.ceil(Math.random() * 100);  
+     let courseDetails = [...this.state.courseDetails, course];
+     console.log('New Course Details Array', courseDetails);
+     // re render happens
+     this.setState({
+      //  courseDetails: newCourseDetails
+        courseDetails
+     })
+   }
+   deleteCourse = (course)=>{
+
+     let courseDetails = [...this.state.courseDetails]
+
+     let newCourse = courseDetails.filter(element=>{
+       if(element.id==course){
+       }
+       else{
+         return element
+
+       }
+     })
+     this.setState({
+       courseDetails: newCourse
+     })
+     ;
+     
+   }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar heading={this.state.heading}></Navbar>
+        <Display courseDetails={this.state.courseDetails} deleteCourse={this.deleteCourse}></Display>
+        <AddCourse addCourse ={this.addCourse}></AddCourse>
+      </div>
+    );
+  }  
+}
+
+export default App;
